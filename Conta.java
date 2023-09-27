4eb2c083f7e99ac72bf15d43522085a7a8a869f9
Lucas Andrade Torres <127760173+Andrade098@users.noreply.github.com>
2023-09-27 15:25:02 -0300

    Create Conta.java

---
+++ Conta.java
@@ -0,0 +1,119 @@
+package model;
+
+import helper.Utils;
+
+public class Conta {
+        private static int codigo = 1001;
+
+        private int numero;
+        private Cliente cliente;
+        private Double saldo = 0.0;
+        private Double limite = 0.0;
+        private Double saldoTotal;
+        
+
+        
+        public Conta(Cliente cliente) {
+            this.numero = Conta.codigo;
+            this.cliente = cliente;
+            Conta.codigo += 1;
+            this.atualizaSaldoTotal();
+        }
+        public Cliente getCliente(){
+            return cliente;
+        }
+
+        public void setCliente(Cliente cliente){
+            this.cliente = cliente;
+        }
+
+        public Double getSaldo() {
+            return saldo;
+        }
+
+        public void setSaldo(Double saldo){
+            this.saldo = saldo;
+        }
+
+        public Double getLimite() {
+            return limite;
+        }
+
+        public int getNumero() {
+            return numero;
+        }
+
+        public void setlimite(Double limite) {
+            this.limite = limite;
+            this.atualizaSaldoTotal();
+        }
+
+        public Double getSaldoTotal() {
+            return saldoTotal;
+        }
+
+        private void atualizaSaldoTotal() {
+            this.saldoTotal = this.getSaldo() + this.getLimite();
+        }
+
+        public String toString() {
+            return "Número da Conta: " + this.getNumero() +
+            "\nCliente: " + this.cliente.getNome() +
+            "\nSaldo Total: " + Utils.doubleParaString(this.getSaldoTotal());
+        }
+
+        public void depositar(Double valor) {
+            if(valor > 0) {
+                this.saldo = this.getSaldo() + valor;
+                this.atualizaSaldoTotal();
+                System.out.println("Depósito efetuado com sucesso!");
+                System.out.println();
+            }else {
+                System.out.println("Erro ao efetuar depósito. Tente novamente.");
+            }
+        }
+
+        public void sacar(Double valor) {
+            Double saldoAtual = this.getSaldo();
+            Double limiteAtual = this.getLimite();
+        
+            if (valor > 0 && saldoAtual + limiteAtual >= valor) {
+                if (saldoAtual >= valor) {
+                    this.saldo -= valor;
+                } else {
+                    Double restante = saldoAtual - valor;
+                    this.limite += restante;
+                    this.saldo = 0.0;
+                }
+                this.atualizaSaldoTotal();
+                System.out.println("Saque efetuado com sucesso!");
+            } else {
+                System.out.println("Saque não realizado. Tente novamente.");
+            }
+        }
+        
+
+    public void transferir(Conta destino, Double valor) {
+        if(valor > 0 && this.getSaldoTotal() >= valor) {
+            if(this.getSaldo() >= valor) {
+                this.saldo = this.getSaldo() - valor;
+                destino.saldo = destino.getSaldo() + valor;
+                this.atualizaSaldoTotal();
+                destino.atualizaSaldoTotal();
+                System.out.println("Transferência realizada com sucesso!");
+            }else{
+                Double restante = this.getSaldo() - valor;
+                this.limite = this.getLimite() + restante;
+                this.saldo = 0.0;
+                destino.saldo = destino.getSaldo() + valor;
+                this.atualizaSaldoTotal();
+                destino.atualizaSaldoTotal();
+                System.out.println("Tranferência realizada com sucesso!");
+            }
+        }else {
+            System.out.println("Transferência não realizada. Tente novamente.");
+        }
+    }
+
+
+}
